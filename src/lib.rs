a7dd5b322fd47d19c1e004542e50b2a4ae205b79
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-02-13 17:49:23 +0100

    Add empty BRecord struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,22 @@
+use std::str::FromStr;
+
+struct BRecord {
+}
+
+impl FromStr for BRecord {
+    type Err = ();
+    fn from_str(s: &str) -> Result<BRecord, ()> {
+        Ok(BRecord {})
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use BRecord;
+
     #[test]
     fn it_works() {
+        let record = "B1414065016925N00953112EA021640228700309".parse::<BRecord>();
+        assert!(record.is_ok());
     }
 }
