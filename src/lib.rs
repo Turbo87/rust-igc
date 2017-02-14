d9c31d4653ab4760006635ede3d25a262a03c99d
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-02-14 08:38:31 +0100

    Parse complete B record

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,25 @@ use regex::Regex;
 struct BRecord {
     time: NaiveTime,
     location: Point<f64>,
+    valid: bool,
+    pressure_altitude: i16,
+    gps_altitude: i16,
+    extra: String,
 }
 
 impl FromStr for BRecord {
     type Err = String;
     fn from_str(s: &str) -> Result<BRecord, String> {
-        let re = Regex::new(r"^B(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d{3})([NS])(\d{3})(\d{2})(\d{3})([EW])").unwrap();
+        let re = Regex::new(concat!(r"^B",
+            r"(\d{2})(\d{2})(\d{2})",
+            r"(\d{2})(\d{2})(\d{3})([NS])",
+            r"(\d{3})(\d{2})(\d{3})([EW])",
+            r"([AV])",
+            r"([-\d]\d\d\d\d)",
+            r"([-\d]\d\d\d\d)",
+            r"([0-9a-zA-Z]*).*$",
+        )).unwrap();
+
         let caps = try!(re.captures(s).ok_or("Could not parse B record"));
 
         let time = try!(NaiveTime::from_hms_opt(
@@ -40,9 +53,18 @@ impl FromStr for BRecord {
 
         let location = Point::new(longitude, latitude);
 
+        let valid = caps.get(12).unwrap().as_str() == "A";
+        let pressure_altitude = caps.get(13).unwrap().as_str().parse::<i16>().unwrap();
+        let gps_altitude = caps.get(14).unwrap().as_str().parse::<i16>().unwrap();
+        let extra = caps.get(15).unwrap().as_str().to_string();
+
         Ok(BRecord {
             time: time,
             location: location,
+            valid: valid,
+            pressure_altitude: pressure_altitude,
+            gps_altitude: gps_altitude,
+            extra: extra,
         })
     }
 }
@@ -63,5 +85,9 @@ mod tests {
         let record: BRecord = "B1414065016925N00953112EA021640228700309".parse().unwrap();
         assert_eq!(record.time, NaiveTime::from_hms(14, 14, 06));
         assert_eq!(record.location, Point::new(9.8852, 50.28208333333333));
+        assert_eq!(record.valid, true);
+        assert_eq!(record.pressure_altitude, 2164);
+        assert_eq!(record.gps_altitude, 2287);
+        assert_eq!(record.extra, "00309");
     }
 }
