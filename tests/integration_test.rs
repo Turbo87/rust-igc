a60f89eaece5824caaa21c0b9004d058463ca268
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-06-19 18:20:38 +0200

    tests: Count types of records

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -16,9 +16,63 @@ fn it_works() {
     let file = File::open(path).unwrap();
     let buf_reader = BufReader::new(file);
 
-    let records: Vec<Record> = parse(buf_reader)
-        .filter_map(Result::ok)
-        .collect();
+    let mut result_count = 0;
+    let mut error_count = 0;
+    let mut record_count = 0;
+    let mut a_record_count = 0;
+    let mut b_record_count = 0;
+    let mut c_record_count = 0;
+    let mut d_record_count = 0;
+    let mut e_record_count = 0;
+    let mut f_record_count = 0;
+    let mut g_record_count = 0;
+    let mut h_record_count = 0;
+    let mut i_record_count = 0;
+    let mut j_record_count = 0;
+    let mut k_record_count = 0;
+    let mut l_record_count = 0;
 
-    assert_eq!(records.len(), 13533);
+    for result in parse(buf_reader) {
+        result_count += 1;
+
+        match result {
+            Err(_) => error_count += 1,
+            Ok(record) => {
+                record_count += 1;
+
+                match record {
+                    Record::A => a_record_count += 1,
+                    Record::B(_) => b_record_count += 1,
+                    Record::C => c_record_count += 1,
+                    Record::D => d_record_count += 1,
+                    Record::E => e_record_count += 1,
+                    Record::F => f_record_count += 1,
+                    Record::G => g_record_count += 1,
+                    Record::H => h_record_count += 1,
+                    Record::I => i_record_count += 1,
+                    Record::J => j_record_count += 1,
+                    Record::K => k_record_count += 1,
+                    Record::L => l_record_count += 1,
+                }
+            },
+        }
+    }
+
+    assert_eq!(result_count, 13533);
+
+    assert_eq!(error_count, 0);
+    assert_eq!(record_count, 13533);
+
+    assert_eq!(a_record_count, 1);
+    assert_eq!(b_record_count, 9762);
+    assert_eq!(c_record_count, 7);
+    assert_eq!(d_record_count, 0);
+    assert_eq!(e_record_count, 0);
+    assert_eq!(f_record_count, 130);
+    assert_eq!(g_record_count, 2);
+    assert_eq!(h_record_count, 14);
+    assert_eq!(i_record_count, 1);
+    assert_eq!(j_record_count, 0);
+    assert_eq!(k_record_count, 0);
+    assert_eq!(l_record_count, 3616);
 }
