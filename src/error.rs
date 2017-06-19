30973453078b292c3c58664a6f79dc6d563fe9a9
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-06-19 16:50:15 +0200

    Error: Add IoError option

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,12 @@
 use std::error;
 use std::fmt;
+use std::io;
 use std::num;
 use std::string;
 
 #[derive(Debug)]
 pub enum ParseError {
+    IoError(io::Error),
     InvalidCharacters(string::FromUtf8Error),
     InvalidIntNumber(num::ParseIntError),
     LineTooShort,
@@ -18,6 +20,7 @@ pub enum ParseError {
 impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
+            ParseError::IoError(ref err) => err.fmt(f),
             ParseError::InvalidCharacters(ref err) => err.fmt(f),
             ParseError::InvalidIntNumber(ref err) => err.fmt(f),
             ParseError::LineTooShort => write!(f, "Line too short"),
@@ -33,6 +36,7 @@ impl fmt::Display for ParseError {
 impl error::Error for ParseError {
     fn description(&self) -> &str {
         match *self {
+            ParseError::IoError(ref err) => err.description(),
             ParseError::InvalidCharacters(ref err) => err.description(),
             ParseError::InvalidIntNumber(ref err) => err.description(),
             ParseError::LineTooShort => "Line too short",
@@ -46,10 +50,17 @@ impl error::Error for ParseError {
 
     fn cause(&self) -> Option<&error::Error> {
         match *self {
+            ParseError::IoError(ref err) => Some(err),
             ParseError::InvalidCharacters(ref err) => Some(err),
             ParseError::InvalidIntNumber(ref err) => Some(err),
             _ => None,
         }
+    }
+}
+
+impl From<io::Error> for ParseError {
+    fn from(err: io::Error) -> ParseError {
+        ParseError::IoError(err)
     }
 }
 
